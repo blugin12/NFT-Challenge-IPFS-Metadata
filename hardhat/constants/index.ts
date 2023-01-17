b73e78b90c8f85007daecf113d99c4e2af25c3db
laykimleng <bbottra12@gmail.com>
2023-01-17 11:08:42 +0700

    new

---
+++ hardhat/constants/index.ts
@@ -0,0 +1,11 @@
+// Address of the Whitelist Contract that you deployed
+// export const WHITELIST_CONTRACT_ADDRESS = "0x1e3c83FA89C426221103F3f38B779c941B46c860";
+export const WHITELIST_CONTRACT_ADDRESS = "0xc36E7A702a73D20a12F9B991C4562Af4C970103B";
+// URL to extract Metadata for a Crypto Dev NFT
+// const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+export const baseURI = "ipfs://QmRiNqFUk5MFMpw8RKfufbk6nEBoDiXWjNax3CVotSLNZv/";
+export const CONTRACT_URI = "ipfs://QmRiNqFUk5MFMpw8RKfufbk6nEBoDiXWjNax3CVotSLNZv/1";
+export const artist = "0xef9E76a5A3d4dF6B364F0C6675eC70c414Ff4222";
+export const royalityFee = 500;
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, baseURI, royalityFee,artist};
